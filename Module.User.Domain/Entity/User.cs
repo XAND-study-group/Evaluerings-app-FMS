8e9513bef81da317126ed99a73ee93d286ef3216
xabur95 <148750333+xabur95@users.noreply.github.com>
2024-11-07 17:32:07 +0100

    User.Domain.Entity has been added

---
+++ Module.User.Domain/Entity/User.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module.User.Domain.Entity
+{
+    public class User
+    {
+        public Guid Id { get; protected set; }
+
+        public string Firstname { get; protected set; }
+        public string Lastname { get; protected set; }
+        public string Email { get; protected set; }
+
+        public IEnumerable<Seminar> Seminars { get; protected set; }
+
+
+        protected User()
+        {
+        }
+
+        private User(string firstname, string lastname, string email)
+        {
+            Firstname = firstname;
+            Lastname = lastname;
+            Email = email;
+        }
+
+
+
+        // Hvorfor er denne metode lavet som en Lambda? 
+        public static User Create(string firstname, string lastname, string email)=>        
+            new User(firstname, lastname, email);
+        
+
+        public void Update(string firstname, string lastname, string email)
+        {
+            Firstname = firstname;
+            Lastname = lastname;
+            Email = email;
+        }
+
+
+    }
+}
