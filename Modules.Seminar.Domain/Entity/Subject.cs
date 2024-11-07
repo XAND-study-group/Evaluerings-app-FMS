a27d8a99303053810c2abe550f69eb12cfb03faa
Niller2503 <niklaspetersen441@gmail.com>
2024-11-07 22:19:07 +0100

    Entity Done

--- Modules.Seminar.Domain/Entity/Subject.cs
+++ Modules.Seminar.Domain/Entity/Subject.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Module.Seminar.Domain.Entity
 {
-    internal class Subject
+    public class Subject
     {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+
+        public Subject(int id, string name, string description)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+        }
+        public Subject() { }
     }
 }
