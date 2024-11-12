5ca937aa387cc61ed3133d2ecfc01e75f2db02eb
xabur95 <148750333+xabur95@users.noreply.github.com>
2024-11-12 22:50:38 +0100

    User Backend Done (No Tests Done Yet) Endpoints are set up

---
+++ Module.User/Endpoints/User/GetUsers.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Module.Shared.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Module.User.Application.Features.User.Query;
+
+namespace Module.User.Endpoints.UserManagement
+{
+    public class GetUsers : IEndpoint
+    {
+        void IEndpoint.MapEndpoint(WebApplication app)
+        {
+            app.MapGet("User", async ([FromServices] IMediator mediator)
+                  => await mediator.Send(new GetUsersQuery()));
+        }
+    }
+}
