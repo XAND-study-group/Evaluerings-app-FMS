341f2d3a02112e987fd78099cc534f5338664dd6
xabur95 <148750333+xabur95@users.noreply.github.com>
2024-11-13 08:46:47 +0100

    User tests

---
+++ Module.User.Domain.Test/Fakes/FakeUser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module.User.Domain.Test.Fakes
+{
+    public class FakeUser : Entities.User
+    {
+        public FakeUser(string firstname, string lastname, string email)
+        {
+            Firstname = firstname;
+            Lastname = lastname;
+            Email = email;
+        }
+
+
+        public new void ValidateName(string name)
+            => base.ValidateName(name);
+
+        public new void ValidateEmail(string email)
+            => base.ValidateEmail(email);
+
+    }
+}
