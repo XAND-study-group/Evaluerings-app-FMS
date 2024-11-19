f44b098bb16e2e00154991b7030adeba3a1091a0
AndreasOEK <oe.thomsen@gmail.com>
2024-11-19 08:41:25 +0100

    .

--- Module.Feedback.Domain/Extensions/VoteExtension.cs
+++ Module.Feedback.Domain/Extensions/VoteExtension.cs
@@ -4,8 +4,8 @@ namespace Module.Feedback.Domain.Extensions;
 
 public static class VoteExtension
 {
-    public static GetVoteResponse MapToGetVoteResponse(this Vote vote)
-        => new GetVoteResponse(vote.Id, vote.RowVersion, vote.HashedId, vote.VoteScale);
+    public static GetVoteResponse MapToGetVoteResponse(this Vote vote) =>
+        new(vote.Id, vote.RowVersion, vote.HashedId, vote.VoteScale);
     
     public static IEnumerable<GetVoteResponse> MapToIEnumerableGetVoteResponse(this IEnumerable<Vote> votes)
         => votes.Select(vote => vote.MapToGetVoteResponse());
