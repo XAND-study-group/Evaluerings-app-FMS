a9d83fa3d0b679292462a727e0bd4350b5033a11
AndreasOEK <oe.thomsen@gmail.com>
2024-11-27 22:16:38 +0100

    Added Endpoint RemoveClassIdFromRoom.cs [DONE], SubscribeToRoomNotification.cs [DONE], UnsubscribeFromRoomNotification.cs [DONE]

---
+++ Module.Feedback.Application/Features/Room/Command/RemoveClassIdFromRoomCommand.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Module.Feedback.Application.Abstractions;
+using SharedKernel.Dto.Features.Evaluering.Room.Command;
+using SharedKernel.Models;
+
+namespace Module.Feedback.Application.Features.Room.Command;
+
+public record RemoveClassIdFromRoomCommand(RemoveClassIdFromRoomRequest RemoveClassIdFromRoomRequest) : IRequest<Result<bool>>;
+
+public class RemoveClassIdFromRoomCommandHandler(IRoomRepository roomRepository) : IRequestHandler<RemoveClassIdFromRoomCommand, Result<bool>>
+{
+    public async Task<Result<bool>> Handle(RemoveClassIdFromRoomCommand request, CancellationToken cancellationToken)
+    {
+        try
+        {
+
+            // Load
+            var removeClassIdFromRoomRequest = request.RemoveClassIdFromRoomRequest;
+            var room = await roomRepository.GetRoomByIdAsync(removeClassIdFromRoomRequest.RoomId);
+        
+            // Do
+            room.RemoveClassId(removeClassIdFromRoomRequest.ClassId);
+        
+            // Save
+            await roomRepository.UpdateRoomAsync(room, removeClassIdFromRoomRequest.RowVersion);
+
+            return Result<bool>.Create("Klasse fjernet fra forum", true, ResultStatus.Success);
+        }
+        catch (Exception e)
+        {
+            return Result<bool>.Create(e.Message, false, ResultStatus.Error);
+        }
+    }
+}
---
+++ SharedKernel.Dto/Features/Evaluering/Room/Command/RemoveClassIdFromRoomRequest.cs
@@ -0,0 +1,6 @@
+﻿namespace SharedKernel.Dto.Features.Evaluering.Room.Command;
+
+public record RemoveClassIdFromRoomRequest(
+    Guid RoomId,
+    Guid ClassId,
+    byte[] RowVersion);
