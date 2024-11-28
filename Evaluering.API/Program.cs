3f04ac04eee51ca34008b2a4158e4b5bf0ed0ca3
Daniel From Kjærgaard <KongzCringe@gmail.com>
2024-11-28 15:52:25 +0100

    Authentication added to evaluationApp (Needs testing)

--- Evaluering.API/Program.cs
+++ Evaluering.API/Program.cs
@@ -1,13 +1,29 @@
+using System.Text;
 using Evaluering.API;
 using MediatR;
 using Evaluering.API.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using Module.Feedback.Domain.DomainServices.Interfaces;
 using Module.Feedback.Extension;
 using Module.Feedback.Infrastructure.Options;
 using School.API.Helper;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(o =>
+    {
+        o.RequireHttpsMetadata = false;
+        o.TokenValidationParameters = new TokenValidationParameters
+        {
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Secret"])),
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            ClockSkew = TimeSpan.Zero
+        };
+    });
 
 builder.Services.AddAuthorizationWithPolicies();
 // Add services to the container.
