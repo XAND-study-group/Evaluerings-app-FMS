b508779ce75139cfafc940127c7af92cad3d33a8
AndreasOEK <oe.thomsen@gmail.com>
2024-12-07 22:00:20 +0100

    Add line 65-72 in Program.cs

--- Evaluering.API/Program.cs
+++ Evaluering.API/Program.cs
@@ -61,6 +61,16 @@ if (app.Environment.IsDevelopment())
 
 // app.UseHttpsRedirection();
 
+// Enable request body buffering 
+app.Use(async (context, next) =>
+{
+    context.Request.EnableBuffering();
+    await next();
+});
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapPost("TestGemini/Feedback",
     async ([FromBody] FeedbackContentDto content, [FromServices] IValidationServiceProxy validationServiceProxy) =>
     await validationServiceProxy.IsAcceptableContentAsync(content.Title, content.Problem, content.Solution));
